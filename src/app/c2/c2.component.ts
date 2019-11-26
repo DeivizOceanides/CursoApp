9a139a5c5e5e0b44a7f33159a5fe592ebb27a8b7
DeivizOceanides <david.martin.martinez@gmail.com>
2019-11-26 11:16:15 +0100

    Dia 2.1

--- src/app/c2/c2.component.ts
+++ src/app/c2/c2.component.ts
@@ -6,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./c2.component.css']
 })
 export class C2Component implements OnInit {
+  varBolean: boolean;
+  varSwitch: number;
+  varFor: number[];
+  numeroPipe = 10.55;
+  dineroPipe = 13.75;
+  fechaPipe = Date.now();
+  objetoPipe = {
+    nombre: 'curso',
+    asistentes: 12
+  };
 
-  constructor() { }
+  constructor() {
+    this.varBolean = true;
+    this.varSwitch = 1;
+    this.varFor=[1,2,3,4,5,6];
+  }
 
   ngOnInit() {
   }
